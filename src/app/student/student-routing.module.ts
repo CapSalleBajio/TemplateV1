cf5e903f4c7f80e3cf447ca7429cbb31835c6e82
omarsalas94 <omar-salas@outlook.com>
2020-10-20 11:14:17 -0500

    rutas

--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { NewComponent } from './new/new.component';
+import { TemplateComponent } from './template/template.component';
 
 const routes: Routes =  [
     {path: 'list', component: ListComponent},
     {path: 'new', component: NewComponent},
+    {path: 'tpl', component: TemplateComponent, children: [
+        {path: 'list', component: ListComponent},
+        {path: 'new', component: NewComponent},
+        {path: '**', redirectTo: 'list'}
+    ]},
     {path: '**', redirectTo: 'list'}
 ];
 
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { NewComponent } from './new/new.component';
 import { StudentRoutingModule } from './student-routing.module';
+import { TemplateComponent } from './template/template.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [ListComponent, NewComponent],
+  declarations: [ListComponent, NewComponent, TemplateComponent],
   imports: [
     CommonModule,
     StudentRoutingModule,
+    RouterModule,
   ]
 })
 export class StudentModule { }
