06cad6da1517c700acf3a304a4061c7ab4a9a4b4
omar salas rodriguez <omar-salas@outlook.com>
2020-12-04 11:18:10 -0600

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
-  SERVER_URL: 'https://localhost:3300'
-
+  SERVER_URL: 'http://165.22.15.237:3000',
+  firebaseConfig: {
+    apiKey: 'AIzaSyCl_KBZtn5AQyIZuUaQslsIRd9J5bHscZE',
+    authDomain: 'salle-app-592bb.firebaseapp.com',
+    databaseURL: 'https://salle-app-592bb.firebaseio.com',
+    projectId: 'salle-app-592bb',
+    storageBucket: 'salle-app-592bb.appspot.com',
+    messagingSenderId: '995597875367',
+    appId: '1:995597875367:web:64dfc0c9bee68225d18382'
+  }
 };
